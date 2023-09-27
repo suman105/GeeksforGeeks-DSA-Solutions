b75dbd06c37514c1ded277ebfb8e94aa07cb048c
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 15:22:10 -0500

    Added Solution - GfG to GitHub

---
+++ Medium/Sort an array according to the other/sort-an-array-according-to-the-other.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template in C++
+
+
+class Solution{
+    public:
+    vector<int> sortA1ByA2(vector<int> A1, int N, vector<int> A2, int M) 
+    {
+        vector<int> v;
+        unordered_map<int,int> u;
+        sort(A1.begin(),A1.end());
+        for(int i=0;i<A1.size();i++)
+        {
+            if(u.find(A1[i])==u.end())
+                u[A1[i]] = 1;
+            else
+                u[A1[i]]+=1;
+        }
+        for(int i=0;i<A2.size();++i)
+        {
+            if(u.find(A2[i])!=u.end())
+            {
+                while(u[A2[i]])
+                {
+                    v.push_back(A2[i]);
+                    --u[A2[i]];
+                }
+            }
+        }
+        u.clear();
+        for(int i=0;i<A2.size();i++)
+        {
+            if(u.find(A2[i])==u.end())
+                u[A2[i]]=1;
+            else
+                u[A2[i]]+=1;
+        }
+        for(int i=0;i<A1.size();i++)
+        {
+            if(u.find(A1[i])==u.end())
+                v.push_back(A1[i]);
+        }
+        return v;
+    } 
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(int argc, char *argv[]) 
+{ 
+	
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    vector<int> a1(n);
+	    vector<int> a2(m);
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> a1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> a2[i];
+	    }
+	    
+	    Solution ob;
+	    a1 = ob.sortA1ByA2(a1, n, a2, m); 
+	
+	   
+	    for (int i = 0; i < n; i++) 
+		    cout<<a1[i]<<" ";
+		
+	    cout << endl;
+	    
+	    
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
