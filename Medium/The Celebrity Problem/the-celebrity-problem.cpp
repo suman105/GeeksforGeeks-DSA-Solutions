0e5d4e96c9e907b91d108296f0eb560121513f3e
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 14:07:53 -0500

    Added Solution - GfG to GitHub

---
+++ Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> st;
+        for(int i=0;i<n;i++)
+            st.push(i);
+        while(st.size()>1)
+        {
+            int first = st.top();
+            st.pop();
+            int second = st.top();
+            st.pop();
+            if(M[first][second]==1)  
+                st.push(second);
+            else 
+                st.push(first);
+        }
+        
+        int final=st.top();
+        st.pop();
+        for(int i=0;i<n;i++)
+        {
+            if(final==i)    continue;
+            if(M[final][i]!=0 || M[i][final]!=1)    return -1;
+        }
+        return final;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
