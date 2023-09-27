e77df49b0e002693bdf8b265b2a8e2b333df357b
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 13:07:43 -0500

    Added Solution - GfG to GitHub

---
+++ Hard/Sorted Link List to BST/sorted-link-list-to-bst.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct LNode
+{
+    int data;
+    struct LNode* next;
+    
+    LNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TNode  
+{  
+    
+    int data;  
+    struct TNode* left;  
+    struct TNode* right; 
+    TNode(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+}; 
+
+void preOrder(TNode* node)  
+{  
+    if (node == NULL)  
+        return;  
+    cout<<node->data<<" ";  
+    preOrder(node->left);  
+    preOrder(node->right);  
+} 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+//User function Template for C++
+
+/* 
+//Linked List
+struct LNode
+{
+    int data;
+    struct LNode* next;
+    
+    LNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+//Tree
+struct TNode  
+{  
+    
+    int data;  
+    struct TNode* left;  
+    struct TNode* right; 
+    TNode(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+}; */
+class Solution{
+  public:
+    
+    LNode* middle(LNode *head)
+    {
+        LNode *prev=NULL;
+        LNode *slow=head;
+        LNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        prev->next = NULL;
+        return slow;
+    }
+    
+    TNode* sortedListToBST(LNode *head)
+    {
+        if(head==NULL)
+            return NULL;
+        
+        if(head->next==NULL)
+            return new TNode(head->data);
+        
+        LNode* mid = middle(head);
+        TNode* root = new TNode(mid->data);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+        
+        return root;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        LNode *head = new LNode(data);
+        LNode *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new LNode(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        TNode* Thead = ob.sortedListToBST(head);
+        preOrder(Thead);
+        cout<<"\n";
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
