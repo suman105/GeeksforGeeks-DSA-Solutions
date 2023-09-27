d161b07f4790ac8146880c2319c2de05715dd3ef
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 14:24:46 -0500

    Added Solution - GfG to GitHub

---
+++ Medium/Check if subtree/check-if-subtree.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+class Solution
+{
+  public:
+    bool perfect(Node* root, Node* head)
+    {
+        if(!root && !head)  return true;
+        else if((!root && head) || (!head && root)) return false;
+        else if(root->data != head->data)   return false;
+        return perfect(root->left,head->left) && perfect(root->right,head->right);
+    }
+  
+    void mainfunction(Node *root, Node* head,bool &found)
+    {
+        if(!root)   return;
+        else if(root->data == head->data)
+        {
+            if(perfect(root,head)==true){
+                found=true;
+                return;
+            }
+        }
+        mainfunction(root->left,head,found);
+        mainfunction(root->right,head,found);
+    }
+    
+    bool isSubTree(Node* T, Node* S) 
+    {
+        bool found=false;
+        mainfunction(T,S,found);
+        return found;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string strT, strS;
+        getline(cin, strT);
+        Node *rootT = buildTree(strT);
+        getline(cin, strS);
+        Solution obj;
+        Node *rootS = buildTree(strS);
+        cout << obj.isSubTree(rootT, rootS) << "\n";
+
+    }
+    return 0;
+}
+// } Driver Code Ends
