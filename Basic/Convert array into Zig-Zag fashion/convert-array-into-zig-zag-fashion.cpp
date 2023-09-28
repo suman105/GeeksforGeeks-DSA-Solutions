657d9f1fdc995a184cba61eb807df0e41911b710
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 20:50:09 -0500

    Added Solution - GfG to GitHub

---
+++ Basic/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+public:	
+	void zigZag(int arr[], int n) 
+	{
+	    int i=0,j=1;
+	    for(int k=0;k<n;k++)
+	    {
+	        if(arr[i]>arr[j])
+	            swap(arr[i],arr[j]);
+	        ++i;
+	        ++j;
+	        if(j>=n)
+	            break;
+	        if(arr[i]<arr[j])
+	            swap(arr[i],arr[j]);
+	       ++i;++j;
+	        if(j>=n)
+	            break;
+	    }
+	}
+};
+
+
+
+//{ Driver Code Starts.
+
+bool isZigzag(int arr[], int n){
+    int f=1;
+    
+    for(int i=1; i<n; i++){
+        if(f){
+            if(arr[i-1]>arr[i]) return 0;
+        }
+        else{
+            if(arr[i-1]<arr[i]) return 0;
+        }
+        f=f^1;
+    }
+    
+    return 1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        bool check=1;
+        check=isZigzag(arr,n);
+        
+        if(check) cout<<"1\n";
+        else cout<<"0\n";
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
