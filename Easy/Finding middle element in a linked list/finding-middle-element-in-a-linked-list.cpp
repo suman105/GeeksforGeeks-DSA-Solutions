aa19e67c23374a49e1898fb476bc22751f51ae6a
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-28 11:16:03 -0500

    Updated Solution - GfG to GitHub

--- Easy/Finding middle element in a linked list/finding-middle-element-in-a-linked-list.cpp
+++ Easy/Finding middle element in a linked list/finding-middle-element-in-a-linked-list.cpp
@@ -25,17 +25,7 @@ void printList(Node* node)
 
 
 // } Driver Code Ends
-/* Link list Node 
-struct Node {
-    int data;
-    Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-    
-}; */
+
 class Solution{
     public:
     /* Should return data of middle node. If linked list is empty, then  -1*/
@@ -51,6 +41,7 @@ class Solution{
         return slow->data;
     }
 };
+
 
 
 
