61d44dfe491e22c33a39fe2c6040d230945cf7b1
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-28 15:00:30 -0500

    Added Solution - GfG to GitHub

---
+++ Easy/Count the Specials/count-the-specials.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function Prototype
+int countSpecials(int[], int, int);
+
+
+// } Driver Code Ends
+int countSpecials(int arr[], int sizeof_array, int K){
+    int f = floor(sizeof_array/K), count = 0;
+    multiset<int> m;
+    set<int> s;
+    int i=0;
+    for(i=0;i<sizeof_array;i++)
+        m.insert(arr[i]);
+    for(i=0;i<sizeof_array;i++)
+    {
+        if(m.count(arr[i]) == f)
+            s.insert(arr[i]);
+    }
+    for(auto &p : s)
+        ++count;
+    
+    return count;
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver code to rest countSpecials function
+int main() {
+	
+	// Testcase input
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    
+	    // sizeof_array : number of array elements
+	    // K : as given in statement
+	    int sizeof_array, K;
+	    cin >> sizeof_array >> K;
+	    
+	    // array of size sizeof_array
+	    int arr[sizeof_array];
+	    
+	    for(int i = 0;i<sizeof_array;i++){
+	        cin >> arr[i];
+	    }
+	    
+	    // calling function and printing count the occurrence
+	    cout << countSpecials(arr, sizeof_array, K) << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
