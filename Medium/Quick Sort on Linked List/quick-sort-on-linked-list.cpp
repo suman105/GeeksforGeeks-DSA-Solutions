1c08f48cec9e53847bf254233ae086d18d33b86f
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-27 19:15:51 -0500

    Added Solution - GfG to GitHub

---
+++ Medium/Quick Sort on Linked List/quick-sort-on-linked-list.cpp
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <cstdio>
+using namespace std;
+ 
+/* a node of the singly linked list */
+struct node
+{
+    int data;
+    struct node *next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct node* new_node = new node(new_data);
+ 
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+ 
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* A utility function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+void quickSort(struct node **headRef);
+int main()
+{
+	int t,n,x;
+	cin>>t;
+	while(t--){
+		
+        
+        cin>>n;n=n-1;
+		cin>>x;
+        node *temp,*head = new node(x);
+        temp=head;
+        while(n--)
+        {
+			cin>>x;
+          temp->next=new node(x);
+          temp=temp->next;
+			}
+    
+    quickSort(&head);
+ 
+    printList(head);
+ while(head!= NULL){
+     temp=head;
+     head=head->next;
+     free(temp);
+     
+ }
+	}
+    return 0;
+}
+// } Driver Code Ends
+
+void swap(int* a, int* b) 
+{ 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+
+int partition (int arr[], int low, int high) 
+{ 
+    int pivot = arr[high];  
+    int i = (low - 1); 
+  
+    for (int j = low; j <= high - 1; j++) 
+    { 
+         
+        if (arr[j] < pivot) 
+        { 
+            i++; 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+  
+
+void quickSort(int arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        
+        int pi = partition(arr, low, high); 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+} 
+  
+
+//you have to complete this function
+void quickSort(struct node **head)
+{
+    int i,a[200],c=0;
+    struct node *t,*l,*m;
+    t = *head;
+    while(t!=NULL){
+        a[c++]=t->data;
+        t=t->next;
+    }
+    quickSort(a,0,c-1);
+    m = new node(a[0]);
+    t=l=m;
+    for(i=1;i<c;i++){
+        m = new node(a[i]);
+        t->next = m;
+        t=m;
+    }
+    *head = l;
+    
+}
