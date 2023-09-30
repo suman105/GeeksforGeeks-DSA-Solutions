e0aaed4b955d95cf54453dd58e8090cf29884175
suman105 <78265708+suman105@users.noreply.github.com>
2023-09-30 13:38:17 -0500

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Array Sorting/binary-array-sorting.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// A Sample C++ program for beginners with Competitive Programming
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    
+    // A[]: input array
+    // N: input array
+    //Function to sort the binary array.
+    void binSort(int A[], int N)
+    {
+       int count0=0,count1=0,i=0;
+       while(i<N){
+           if(A[i]) ++count1;
+           else ++count0;
+           ++i;
+       }
+       i=0;
+       while(count0--)  A[i++]=0;
+       while(count1--)  A[i++]=1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	// Input the number of testcases
+	while(T--)
+	{
+	    int N;
+	    cin>>N; //Input size of array N
+	    int A[N]; 
+	    
+	    for(int i = 0; i  < N; i++)
+	      cin>>A[i];
+	      
+	    Solution obj;
+	    obj.binSort(A,N);
+	    
+	    for(int x:A)
+	    cout<<x<<" ";
+	      
+	    cout<<endl;
+	}
+	return 0;
+}
+
+
+
+
+// } Driver Code Ends
