0cde7cb4ff626dd74ef4921167c7be099853a83e
Suman Majjari <78265708+suman105@users.noreply.github.com>
2023-09-30 13:56:14 -0500

    Update convert-array-into-zig-zag-fashion.cpp

--- Basic/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.cpp
+++ Basic/Convert array into Zig-Zag fashion/convert-array-into-zig-zag-fashion.cpp
@@ -12,12 +12,12 @@ public:
     	{
         	int i=0;
         	while(i<(n-1)){
-            	if(arr[i]>arr[i+1])  swap(arr[i],arr[i+1]);
-            	++i;
-            	if(i<(n-1) && arr[i]<arr[i+1])   swap(arr[i],arr[i+1]);
-            	++i;
-        }
-    }
+	            	if(arr[i]>arr[i+1])  swap(arr[i],arr[i+1]);
+	            	++i;
+	            	if(i<(n-1) && arr[i]<arr[i+1])   swap(arr[i],arr[i+1]);
+	            	++i;
+        	}
+     }
 };
 
 
