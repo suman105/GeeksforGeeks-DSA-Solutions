48bbb25669396de22386dcb7d37f6d2a55596482
Suman Majjari <78265708+suman105@users.noreply.github.com>
2023-09-30 15:17:59 -0500

    Update first-element-to-occur-k-times.cpp

--- Basic/First element to occur k times/first-element-to-occur-k-times.cpp
+++ Basic/First element to occur k times/first-element-to-occur-k-times.cpp
@@ -9,24 +9,13 @@ class Solution{
     int firstElementKTime(int a[], int n, int k)
     {
         unordered_map<int,int> m;
-
         for(int i=0;i<n;i++)
         {
-            if(m.find(a[i])==m.end()){
-                m[a[i]]=1;
-                if(m[a[i]]==k)
+            if(m.find(a[i])==m.end())   m[a[i]]=1;
+            else    ++m[a[i]];
+            if(m[a[i]]==k)
                     return a[i];
-            }
-            else
-            {
-                ++m[a[i]];
-                
-                if(m[a[i]]==k)
-                    return a[i];
-            }
-        }
-        
-        
+        }   
         return -1;
     }
 };
