b8b6c918848860469214106516193a9c214dda9a
Suman Majjari <78265708+suman105@users.noreply.github.com>
2023-09-30 14:42:17 -0500

    Update factorial.cpp

--- Basic/Factorial/factorial.cpp
+++ Basic/Factorial/factorial.cpp
@@ -8,10 +8,7 @@ public:
     long long int factorial(int N){
         int i;
        long long int s=1;
-       for(i=1;i<=N;i++)
-       {
-           s = s*i;
-       }
+       for(i=1;i<=N;i++)    s = s*i;
        return s;
     }
 };
