08614ce889c071d1459e18ead6a7452564f1870b
Suman Majjari <78265708+suman105@users.noreply.github.com>
2023-10-01 11:51:56 -0500

    Update third-largest-element.cpp

--- Basic/Third largest element/third-largest-element.cpp
+++ Basic/Third largest element/third-largest-element.cpp
@@ -8,17 +8,22 @@ class Solution{
   public:
     int thirdLargest(int a[], int n)
     {
-         set<int> s(a,a+n);
-         int x=0;
-         for(auto i = s.begin() ; i != s.end() ; ++i){
-             a[x] = *i;++x;
-         }
-         if(n < 3)
-            return -1;
-         else
-            return a[n-3];
+        if(n<3) return -1;
+        int m1=INT_MIN,m2=INT_MIN,m3=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(a[i]>m1){
+                m3=m2;
+                m2=m1;
+                m1=a[i];
+            }
+            else if(a[i]>m2){
+                m3=m2;
+                m2=a[i];
+            }
+            else if(a[i]>m3)    m3=a[i];
+        }
+        return m3;
     }
-
 };
 
 //{ Driver Code Starts.
